9dee511c3cfca003e2fb9dea8ed2b9bb1fb8f565
Chongk3y <jazzerong214@gmail.com>
2025-07-15 12:19:03 +0800

    Initial commit for Render deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WEIMS.wsgi
--- WEIMS/settings.py
+++ WEIMS/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2jm(plx^dayy^=0$!iu=mf%-$wcnrrm8v*%_#-q@0@05f1%=(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
